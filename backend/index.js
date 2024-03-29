cf77f9d7508c34a4838329eeb9477c561bbb59c5
Vaibhav Bansal <vbvbansal@gmail.com>
2024-03-29 16:24:23 +0530

    added default route

--- backend/index.js
+++ backend/index.js
@@ -8,5 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
+app.get("/", (req, res) => {
+  res.json({
+    message: "This is the root route for my fullstack wallet application",
+  });
+});
 
 module.exports = app;
