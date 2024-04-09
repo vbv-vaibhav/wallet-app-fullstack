5f120199203c667a51a2395239419d4021ffcf3e
Vaibhav Bansal <vbvbansal@gmail.com>
2024-04-09 14:23:38 +0530

    added logout button in receipt page

--- frontend/src/pages/Receipt.jsx
+++ frontend/src/pages/Receipt.jsx
@@ -22,11 +22,19 @@ const Receipt = () => {
       });
   }, []);
 
-  const handleOnclick = () => {
+  const sendAgain = () => {
     setTimeout(() => {
       navigate("/dashboard");
     }, 1000);
   };
+
+  const logOut = () => {
+    setTimeout(() => {
+      navigate("/signin");
+      localStorage.removeItem("token");
+    }, 1000);
+  };
+
   return (
     <div className="flex justify-center h-screen bg-gray-100">
       <div className="h-full flex flex-col justify-center">
@@ -38,19 +46,26 @@ const Receipt = () => {
           </div>
           <div className="px-6 pb-6">
             <h3 className="text-2xl font-semibold text-center">
-              Updated Balance-
+              Updated Balance
             </h3>
-            <h3 className="text-2xl font-semibold text-center pb-6">
+            <h3 className="text-2xl font-semibold text-center pb-6 mt-3 hover:scale-110 duration-300">
               ₹ {balance}
             </h3>
             <div className="space-y-4">
-              <div className="space-y-2"></div>
-              <button
-                onClick={handleOnclick}
-                className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white hover:bg-green-600 duration-300"
-              >
-                Send Again
-              </button>
+              <div className="flex">
+                <button
+                  onClick={sendAgain}
+                  className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors mx-3 h-10 px-4 py-2 w-full bg-green-500 text-white hover:bg-green-600 duration-300"
+                >
+                  Send Again
+                </button>
+                <button
+                  onClick={logOut}
+                  className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors mx-3 h-10 px-4 py-2 w-full  text-white bg-gray-800 hover:bg-gray-900 duration-300"
+                >
+                  Logout
+                </button>
+              </div>
             </div>
           </div>
         </div>
